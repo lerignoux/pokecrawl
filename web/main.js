951b60b3b6d85b23b083095cd563e6fec9019452
Laurent Erignoux <lerignoux@gmail.com>
2016-07-23 02:46:09 +0200

    Cleaning web app script

--- web/main.js
+++ web/main.js
@@ -1,44 +1,39 @@
-var mapContainerId = 'mapid'
-var map;
+var mapContainerId = 'mapid',
+    map;
 
 window.onload = function() {
   displayMap(48.84166274475924, 2.362437632607799);
   ajax({
     method: 'GET',
     url: 'result.json'
-  }, displayPokemons, () => { console.error("Could not fetch data") })
-
-  ajax({
-    method: 'GET',
-    url: 'points.json'
-  }, (data) => data.forEach(p => addMarker(map,p)))
-}
+  }, displayPokemons, () => { console.error("Could not fetch data");});
+};
 
 function displayMap(lat, lon) {
-  console.log('display')
-  map = L.map(mapContainerId).setView([lat, lon], 13)
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+  console.log('display');
+  map = L.map(mapContainerId).setView([lat, lon], 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 }
 
 function displayPokemons(data) {
-  console.log(data)
-  map.setView([data.Latitude, data.Longitude], 13)
-  data.Pokemons.forEach(p => addPokemonMarker(map, p))
+  console.log(data);
+  map.setView([data.Latitude, data.Longitude], 13);
+  data.Pokemons.forEach(p => addPokemonMarker(map, p));
 }
 
 function addPokemonMarker(map, markerInfo) {
-  var marker = L.marker([markerInfo.Latitude, markerInfo.Longitude]).addTo(map)
-  var expiration = markerInfo.Expiration ? new Date(markerInfo.Expiration) : undefined
+  var marker = L.marker([markerInfo.Latitude, markerInfo.Longitude]).addTo(map);
+  var expiration = markerInfo.Expiration ? new Date(markerInfo.Expiration) : undefined;
   marker.bindPopup(
     "<img style='width:40px;' src='https://img.pokemondb.net/artwork/" + markerInfo.Name.toLowerCase() + ".jpg'/><b>" + markerInfo.Name +  "</b> # " + markerInfo.Number + "<br>" +
     (expiration ? "Expiration: " + expiration.toLocaleTimeString() : "")
-  ).openPopup()
+  ).openPopup();
 }
 
 
 function addMarker(map, markerInfo) {
   console.log(markerInfo);
-  var marker = L.marker([markerInfo.Latitude, markerInfo.Longitude]).addTo(map)
+  var marker = L.marker([markerInfo.Latitude, markerInfo.Longitude]).addTo(map);
 }
 
 function ajax(endpoint, success, error) {
@@ -49,7 +44,7 @@ function ajax(endpoint, success, error) {
       var response = null;
       try {
         response = JSON.parse(xhr.responseText);
-      } catch (error) { console.error(error) }
+      } catch (error) { console.error(error);}
       if (response !== null) success(response);
       else error !== undefined && error();
     } else error !== undefined && error();
